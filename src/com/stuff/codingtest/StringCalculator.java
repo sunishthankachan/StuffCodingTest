50ac666d83016d0551750c8fd1d5b966ba32b0a9
sunishthankachan <sunishthankachan@gmail.com>
2023-05-03 08:51:38 +1200

    Updated by refactoring the code with comments and improvements

--- src/com/stuff/codingtest/StringCalculator.java
+++ src/com/stuff/codingtest/StringCalculator.java
@@ -3,11 +3,22 @@ package com.stuff.codingtest;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 
+ * @author Sunish Thankachan
+ * 
+ *         String calculator class that takes a string of numbers as input and
+ *         returns their sum based on some predefined conditions to
+ *         allow/restrict like delimiter type, negative values, greater than a
+ *         particular amount etc.
+ */
 public class StringCalculator {
 
 	private static final String DEFAULT_DELIMTER = ",";
 	private static final String NEW_LINE_DELIMITER = "\n";
+	private static final String CUSTOM_DELIMITER_STARTER = "//";
 	private static final int MAXIMUM_NUMBER_COUNT = 10;
+	private static final int MAXIMUM_VALUE_ALLOWED = 1000;
 
 	public int add(String numbers) throws Exception {
 		int sum = 0;
@@ -26,11 +37,11 @@ public class StringCalculator {
 
 		// Code snippet to handle custom delimiters
 		String delimiter = DEFAULT_DELIMTER;
-		
+
 		// Checks if custom delimiter is there
-		if (numbers.startsWith("//")) {
+		if (numbers.startsWith(CUSTOM_DELIMITER_STARTER)) {
 			String[] parts = numbers.split(NEW_LINE_DELIMITER, 2);
-			if (parts[0].startsWith("//")) {
+			if (parts[0].startsWith(CUSTOM_DELIMITER_STARTER)) {
 				delimiter = parts[0].substring(2);
 				numbers = parts[1];
 			} else {
@@ -39,16 +50,16 @@ public class StringCalculator {
 		}
 
 		String[] numberArray = numbers.split("[" + delimiter + NEW_LINE_DELIMITER + "]+");
-		if (numberArray != null && numberArray.length < MAXIMUM_NUMBER_COUNT) {
+		if (numberArray != null && numberArray.length <= MAXIMUM_NUMBER_COUNT) {
 
 			List<Integer> negatives = new ArrayList<>();
 			for (String number : numberArray) {
 				int num = Integer.parseInt(number);
-				
+
 				// To handle negatives
 				if (num < 0) {
 					negatives.add(num);
-				} else if (num <= 1000) {
+				} else if (num <= MAXIMUM_VALUE_ALLOWED) { // Ignore if GT 1000
 					sum += num;
 				}
 			}
@@ -65,28 +76,29 @@ public class StringCalculator {
 	}
 
 	/**
-	 * Method to check there is any invalid pattern in the number string
+	 * Method to check there is any invalid pattern in the number string. Checks
+	 * whether adjacent patterns are present
 	 * 
 	 * @param numbers
 	 * @return boolean
 	 */
 	public boolean invalidPatternCheck(String numbers) {
-		boolean flag = false;
+		boolean invalidPatternPresent = false;
 
 		for (char value : numbers.toCharArray()) {
 			if (String.valueOf(value).contentEquals(NEW_LINE_DELIMITER)
 					|| String.valueOf(value).contentEquals(DEFAULT_DELIMTER)) {
 
-				if (flag) {
+				if (invalidPatternPresent) {
 					return true;
 				}
-				flag = true;
+				invalidPatternPresent = true;
 			} else {
-				flag = false;
+				invalidPatternPresent = false;
 			}
 		}
 
-		return flag;
+		return invalidPatternPresent;
 	}
 
 	public static void main(String[] args) {
@@ -97,8 +109,8 @@ public class StringCalculator {
 			System.out.println(calculator.add("1")); // 1
 			System.out.println(calculator.add("1,2")); // 3
 			System.out.println(calculator.add("1\n2,3")); // 6
-			System.out.println(calculator.add("1,2,3,4,5,6,7,8,9,10,11")); // Invalid input
-			System.out.println(calculator.add("1,2,\n,3")); // Invalid pattern
+			System.out.println(calculator.add("1,2,3,4,5,6,7,8,9,10,11")); // Exceeded count 0
+			System.out.println(calculator.add("1,2,\n,3")); // Invalid - Adjacent patterns 0
 			System.out.println(calculator.add("//;\n1;2")); // 3
 			System.out.println(calculator.add("1,1001")); // 1
 			System.out.println(calculator.add("//[***]\n1***2***3")); // 6
